e271cb1be274dddfc81c48ac1516fb3e0f310038
Sami <52921619+sbagh@users.noreply.github.com>
2024-09-19 18:28:26 -0400

    target waf

--- ad-bots/GIVT-bot.js
+++ ad-bots/GIVT-bot.js
@@ -7,7 +7,7 @@
 const axios = require("axios");
 
 // Define target URL for ad clicks
-const TARGET_URL = "http://localhost:4333/ad-click";
+const TARGET_URL = "http://localhost:3200/ad-click";
 
 const performGIVTClick = async (clickCount) => {
    for (let i = 0; i < clickCount; i++) {
@@ -19,6 +19,7 @@ const performGIVTClick = async (clickCount) => {
                   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
             },
          });
+         console.log("response", response.data);
          console.log(`GIVT Click: ${i + 1}, Status: ${response.status}`);
       } catch (error) {
          console.error(`Error performing GIVT click: ${error}`);
--- server1.js
+++ server1.js
@@ -7,34 +7,17 @@ app.use(express.json());
 
 let count = 0;
 
-app.get("/bot", (req, res) => {
-   const headers = req.headers;
-   const userAgent = headers["user-agent"];
-
-   count++;
-
-   console.log("headers:", headers);
-   console.log(`Request count: ${count}`);
-
-   let responseMessage = `Request count: ${count}`;
-
-   // Append bot check result to the response message
-   if (userAgent.includes("bot")) {
-      responseMessage += " - You are a bot!";
-   } else {
-      responseMessage += " - You are not a bot.";
-   }
-
-   // Send the complete response once
-   res.send(responseMessage);
-});
 
 app.get("/ad-click", (req, res) => {
    count++;
    const headers = req.headers;
    const referer = headers["referer"];
+   const userAgent = headers["user-agent"];
+   const ip = req.ip;
 
-   console.log("count:", count);
+   console.log(
+      `Count: ${count}, IP: ${ip}, User-Agent: ${userAgent}, Referer: ${referer}`
+   );
 
    res.send("Ad clicked!");
 });
